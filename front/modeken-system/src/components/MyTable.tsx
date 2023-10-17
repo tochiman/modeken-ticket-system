716bdcbfbb8302cfbccef366ceab60c48af0d8ab
tochiman <developer@tochiman.com>
2023-10-17 15:43:33 +0900

    [Update]WebSocketで通信できるかのテスト

--- front/modeken-system/src/components/MyTable.tsx
+++ front/modeken-system/src/components/MyTable.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import { useEffect, useRef, useState } from 'react';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 
 const columns: GridColDef[] = [
   { field: 'id', headerName: '発券番号', minWidth: 30, sortable: false },
@@ -37,6 +37,38 @@ const rows = [
 ];
 
 export default function DataTable() {
+  const socketRef = useRef<WebSocket>()
+  const [isConnected, setIsConnected] = useState<boolean>(false)
+
+  useEffect(() => {
+    //FQDNはクライアントから見たものを入力
+    socketRef.current = new WebSocket('wss://192.168.0.3:8080/api/v1.0/ws')
+    socketRef.current.onopen = function () {
+      socketRef.current?.send(btoa('user:password'))
+      setIsConnected(true)
+      console.log('Connected')
+    }
+
+    socketRef.current.onclose = function () {
+      console.log('closed')
+      setIsConnected(false)
+    }
+
+    //Connectionをはっているときにメッセージを受信したら実行される
+    socketRef.current.onmessage = function () {
+      //ConnectionがCloseされるため空文字は送信
+      socketRef.current?.send('')
+    }
+
+    //WSがUnmountされされた時の処理
+    return () => {
+      if (socketRef.current == null) {
+        return
+      }
+      socketRef.current?.close()
+    }
+  }, [])
+
   return (
       <DataGrid
         rows={rows}
